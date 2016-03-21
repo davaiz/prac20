17893bc83cf91f6c63f59eb7899091c6e70eae2b
davaiz <davaizilg@gmail.com>
2016-03-21 12:58:21 +0400

    Update 2.py

--- 2.py
+++ 2.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import networkx as nx
 def graf():
     G = {}
-    for i in range(int(input('Количество рёбер в графе'))):
+    for i in range(int(input('Количество рёбер в графе '))):
         a, b = input().split()
         if a not in G:
             G[a] = {b}
@@ -23,7 +23,7 @@ def friends(G, P, start, called):
 P = nx.Graph()
 G = graf()
 called = []
-zero = input('Введите начальную точку')
+zero = input('Введите начальную точку ')
 friends(G, P, zero, called)
 nx.draw(P)
 plt.savefig("simple_path.png") # save as png
