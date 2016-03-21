08d302fca46853aaa0b23160b8902fbad17d7c77
davaiz <davaizilg@gmail.com>
2016-03-21 12:56:50 +0400

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,9 +1,8 @@
-# prac20
 import matplotlib.pyplot as plt
 import networkx as nx
 def graf():
     G = {}
-    for i in range(int(input('Количество рёбер в графе'))):
+    for i in range(int(input('Количество рёбер в графе '))):
         a, b, weight = input().split()
         weight = float(weight)
         if a not in G:
@@ -16,9 +15,20 @@ def graf():
             G[b][a] = weight
     return G
 def dejkstra(G, start):
-    shortest_path = {vertex:float('-inf') for vertex in G}
+    shortest_path = {vertex:float('+inf') for vertex in G}
     shortest_path[start] = 0
     queue = [start]
     while queue:
         current = queue.pop(0)
-        print(current, shortest_path[current])
+        for neighbour in G[current]:
+            offering_shortest_path = shortest_path[current] + G[current][neighbour]
+            if offering_shortest_path < shortest_path[neighbour]:
+                shortest_path[neighbour] = offering_shortest_path
+                queue.append(neighbour)
+    return shortest_path
+G = graf()
+zero = input('Введите начальную точку ')
+shortest_path = dejkstra(G, zero)
+print('Кратчайшие пути из ')
+for vertex in G:
+    print(vertex, shortest_path[vertex])
