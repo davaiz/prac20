e4f560fd4ace53401d643d75d5ac0bc9bf30f7ac
davaiz <davaizilg@gmail.com>
2016-03-27 18:45:42 +0400

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,38 +1,36 @@
 import matplotlib.pyplot as plt
 import networkx as nx
-def graf():
-    G = {}
-    for i in range(int(input('Количество рёбер в графе '))):
-        a, b, weight = input().split()
-        weight = float(weight)
-        if a not in G:
-            G[a] = {b:weight}
-        else:
-            G[a][b] = weight
-        if b not in G:
-            G[b] = {a:weight}
-        else:
-            G[b][a] = weight
-    return G
-def dejkstra(G, start):
-    shortest_path = {vertex:float('+inf') for vertex in G}
-    shortest_path[start] = 0
-    queue = [start]
-    while queue:
-        current = queue.pop(0)
-        for neighbour in G[current]:
-            offering_shortest_path = shortest_path[current] + G[current][neighbour]
-            if offering_shortest_path < shortest_path[neighbour]:
-                P.add_edge(current, neighbour)
-                shortest_path[neighbour] = offering_shortest_path
-                queue.append(neighbour)
-    return shortest_path
-G = graf()
-P = nx.Graph()
-zero = input('Введите начальную точку ')
-finish = input('Введите конечную точку ')
-shortest_path = dejkstra(G, zero)
-print('Кратчайшие пути из ')
-nx.draw(P)
-plt.savefig("simple_path.png") # save as png
-plt.show() # display
+def graph():
+    N = int(input('Количество вершин в графе '))
+    weight = [[float('+inf')]*N for j in range(N)]
+    for i in range(N):
+        weight[i][i] = 0
+    M = int(input('Количество ребер в графе '))
+    for i in range(M):
+        a, b, w = input().split()
+        a, b, w = int(a), int(b), float(w)
+        weight[a][b] = w
+        weight[b][a] = w
+    return weight
+
+def printm(A):
+    for i in range(len(A)):
+        print(*A[i], sep='\t')
+    print()
+
+from copy import deepcopy
+
+def floyd(w):
+    N = len(w)
+    F = deepcopy(w)
+    for k in range(N):
+        for i in range(N):
+            for j in range(N):
+                F[i][j] = min(F[i][j], F[i][k] + F[k][j])
+    return F
+
+
+G = graph()
+printm(G)
+F = floyd(G)
+printm(F)
