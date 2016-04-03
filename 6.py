273f7aaae289ab31a0a1b7e926d042d6b5fea83f
davaiz <davaizilg@gmail.com>
2016-04-03 19:05:15 +0400

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,37 +1,44 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 def graph():
-    input4 = open("./graph_input4.txt","r")
-    list4 = input4.readlines()
-    N = int(list4[0])
-    weight = [[float('+inf')]*N for j in range(N)]
-    for i in range(N):
-        weight[i][i] = 0
-    for i in range():
-        a, b, w = input().split()
-        a, b, w = int(a), int(b), float(w)
-        weight[a][b] = w
-        weight[b][a] = w
-    return weight
+    input3 = open("./graph_input3.txt","r")
+    list3 = input3.readlines()
+    G = {}
+    for i in range(len(list3)):
+        list3[i] = list3[i].rstrip()
+        a, b, weight = list3[i].split()
+        weight = float(weight)
+        if a not in G:
+            G[a] = {b:weight}
+        else:
+            G[a][b] = weight
+        if b not in G:
+            G[b] = {a:weight}
+        else:
+            G[b][a] = weight
+    return G
 
-def printm(A):
-    for i in range(len(A)):
-        print(*A[i], sep='\t')
-    print()
-
-from copy import deepcopy
-
-def floyd(w):
-    N = len(w)
-    F = deepcopy(w)
-    for k in range(N):
-        for i in range(N):
-            for j in range(N):
-                F[i][j] = min(F[i][j], F[i][k] + F[k][j])
-    return F
-
-
+def dejkstra(G, start):
+    shortest_path = {vertex:float('+inf') for vertex in G}
+    shortest_path[start] = 0
+    queue = [start]
+    while queue:
+        current = queue.pop(0)
+        for neighbour in G[current]:
+            offering_shortest_path = shortest_path[current] + G[current][neighbour]
+            if offering_shortest_path < shortest_path[neighbour]:
+                shortest_path[neighbour] = offering_shortest_path
+                queue.append(neighbour)
+    return shortest_path
+def shortest(G, start, finish):
+    D = dejkstra(G, start)
+    t = finish
+    path=[]
+    while t:
+        path.append(t)
+        t = D[t][1]
+    return path[::-1]
 G = graph()
-printm(G)
-F = floyd(G)
-printm(F)
+zero = input('Введите начальную точку ')
+finish = input('Введите конечную точку ')
+print(shortest(G,zero,finish))
