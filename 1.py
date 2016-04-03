06cac0329e6185af0bd49419e73c06694b58f690
davaiz <davaizilg@gmail.com>
2016-04-03 18:01:13 +0400

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,15 +1,17 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 G = nx.Graph()
-M = int(input('Количество рёбер в графе '))
-for i in range(M):
-    a = list(input().split())
+input1 = open("./graph_input1.txt","r")
+list1 = input1.readlines()
+for i in range(len(list1)):
+    list1[i] = list1[i].rstrip()
+    a = list(list1[i].split())
     if len(a) == 2:
         G.add_edge(a[0], a[1])
     elif len(a) == 1:
         G.add_node(a[0])
     else:
-        print('Введите правильно! ') 
+        print('Введите правильно! ')
 nx.draw(G)
 plt.savefig("simple_path.png") # save as png
 plt.show() # display
