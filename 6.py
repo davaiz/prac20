df2782adc191ef8e26468c948bbd09829ad7c95a
davaiz <davaizilg@gmail.com>
2016-05-18 09:06:27 +0300

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,32 +1,44 @@
-def graph_input():
-    N = int(input('Количество вершин в графе: '))
-    weight = [[float('+inf')]*N for j in range(N)]
-    for i in range(N):
-        weight[i][i] = 0
-    M = int(input('Количество ребер в графе: '))
-    for i in range(M):
-        a, b, w = input().split()
-        a, b, w = int(a), int(b), float(w)
-        weight[a][b] = w
-        weight[b][a] = w
-    return weight
-def printm(A):
-    for i in range(len(A)):
-        print(*A[i], sep='\t')
-    print()
-
-from copy import deepcopy
-
-def floyd(w):
-    N = len(w)
-    F = deepcopy(w)
-    for k in range(N):
-        for i in range(N):
-            for j in range(N):
-                F[i][j] = min(F[i][j], F[i][k] + F[k][j])
-    return F
-
-G = graph_input()
-printm(G)
-F = floyd(G)
-printm(F)
+def graf():
+    input3 = open("./input6.txt","r")
+    list3 = input3.readlines()
+    G = {}
+    for i in range(len(list3)):
+        list3[i] = list3[i].rstrip()
+        a, b, weight = list3[i].split()
+        weight = float(weight)
+        if a not in G:
+            G[a] = {b:weight}
+        else:
+            G[a][b] = weight
+        if b not in G:
+            G[b] = {a:weight}
+        else:
+            G[b][a] = weight
+    return G
+def dejkstra(G, start):
+    shortest_path = {vertex:float('+inf') for vertex in G}
+    shortest_path[start] = 0
+    queue = [start]
+    while queue:
+        current = queue.pop(0)
+        for neighbour in G[current]:
+            offering_shortest_path = shortest_path[current] + G[current][neighbour]
+            if offering_shortest_path < shortest_path[neighbour]:
+                shortest_path[neighbour] = offering_shortest_path
+                queue.append(neighbour)
+    return shortest_path
+G = graf()
+start=input('START')
+finish = input('FINISH')                 
+shortest_path = dejkstra(G, start)    
+selection = finish
+way  = [finish]
+while selection!= start:
+       for vertex in G[selection]:
+           if selection!= start:
+               if shortest_path[selection] - G[selection][vertex] == shortest_path[vertex]:
+                   selection = vertex
+                   way.append(selection)
+           else:
+               break
+print(way[::-1])
